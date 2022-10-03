abae6085482e3aa38e3b86654f8bee2340bfba80
zeedhoon <zeedhoon@gmail.com>
2022-10-04 00:52:34 +0900

    ref clear error

--- retest.sh
+++ retest.sh
@@ -21,17 +21,16 @@
 # jdk/internal/reflect/CallerSensitive/CheckCSMs
 
 
-pushd /Users/zeedh/slowcoders/jdk-rtgc/build/macosx-x86_64-client-fastdebug/test-support/jtreg_test_jdk_java_lang_ref_ReachabilityFenceTest_java/scratch/0 && \
+pushd /Users/zeedh/slowcoders/jdk-rtgc/build/macosx-x86_64-client-fastdebug/test-support/jtreg_test_jdk_java_lang_ref_ReferenceEnqueue_java/scratch/0 && \
 HOME=/Users/zeedh \
 JDK8_HOME=/Library/Java/JavaVirtualMachines/adoptopenjdk-16.jdk/Contents/Home \
 LANG=en_US.UTF-8 \
 LC_ALL=C \
 PATH=/bin:/usr/bin:/usr/sbin \
 TEST_IMAGE_DIR=/Users/zeedh/slowcoders/jdk-rtgc/build/macosx-x86_64-client-fastdebug/images/test \
-CLASSPATH=/Users/zeedh/slowcoders/jdk-rtgc/build/macosx-x86_64-client-fastdebug/test-support/jtreg_test_jdk_java_lang_ref_ReachabilityFenceTest_java/classes/0/java/lang/ref/ReachabilityFenceTest.d:/Users/zeedh/slowcoders/jdk-rtgc/test/jdk/java/lang/ref:/Users/zeedh/slowcoders/jdk-rtgc/jtreg-6.1/lib/javatest.jar:/Users/zeedh/slowcoders/jdk-rtgc/jtreg-6.1/lib/jtreg.jar \
     /Users/zeedh/slowcoders/jdk-rtgc/build/macosx-x86_64-client-fastdebug/images/jdk/bin/java \
-        -Dtest.vm.opts='-Xmx768m -XX:MaxRAMPercentage=6.25 -Djava.io.tmpdir=/Users/zeedh/slowcoders/jdk-rtgc/build/macosx-x86_64-client-fastdebug/test-support/jtreg_test_jdk_java_lang_ref_ReachabilityFenceTest_java/tmp -ea -esa' \
-        -Dtest.tool.vm.opts='-J-Xmx768m -J-XX:MaxRAMPercentage=6.25 -J-Djava.io.tmpdir=/Users/zeedh/slowcoders/jdk-rtgc/build/macosx-x86_64-client-fastdebug/test-support/jtreg_test_jdk_java_lang_ref_ReachabilityFenceTest_java/tmp -J-ea -J-esa' \
+        -Dtest.vm.opts='-Xmx768m -XX:MaxRAMPercentage=6.25 -Djava.io.tmpdir=/Users/zeedh/slowcoders/jdk-rtgc/build/macosx-x86_64-client-fastdebug/test-support/jtreg_test_jdk_java_lang_ref_ReferenceEnqueue_java/tmp -ea -esa' \
+        -Dtest.tool.vm.opts='-J-Xmx768m -J-XX:MaxRAMPercentage=6.25 -J-Djava.io.tmpdir=/Users/zeedh/slowcoders/jdk-rtgc/build/macosx-x86_64-client-fastdebug/test-support/jtreg_test_jdk_java_lang_ref_ReferenceEnqueue_java/tmp -J-ea -J-esa' \
         -Dtest.compiler.opts= \
         -Dtest.java.opts= \
         -Dtest.jdk=/Users/zeedh/slowcoders/jdk-rtgc/build/macosx-x86_64-client-fastdebug/images/jdk \
@@ -39,19 +38,12 @@ CLASSPATH=/Users/zeedh/slowcoders/jdk-rtgc/build/macosx-x86_64-client-fastdebug/
         -Dtest.timeout.factor=4.0 \
         -Dtest.nativepath=/Users/zeedh/slowcoders/jdk-rtgc/build/macosx-x86_64-client-fastdebug/images/test/jdk/jtreg/native \
         -Dtest.root=/Users/zeedh/slowcoders/jdk-rtgc/test/jdk \
-        -Dtest.name=java/lang/ref/ReachabilityFenceTest.java \
-        -Dtest.file=/Users/zeedh/slowcoders/jdk-rtgc/test/jdk/java/lang/ref/ReachabilityFenceTest.java \
+        -Dtest.name=java/lang/ref/ReferenceEnqueue.java \
+        -Dtest.file=/Users/zeedh/slowcoders/jdk-rtgc/test/jdk/java/lang/ref/ReferenceEnqueue.java \
         -Dtest.src=/Users/zeedh/slowcoders/jdk-rtgc/test/jdk/java/lang/ref \
         -Dtest.src.path=/Users/zeedh/slowcoders/jdk-rtgc/test/jdk/java/lang/ref \
-        -Dtest.classes=/Users/zeedh/slowcoders/jdk-rtgc/build/macosx-x86_64-client-fastdebug/test-support/jtreg_test_jdk_java_lang_ref_ReachabilityFenceTest_java/classes/0/java/lang/ref/ReachabilityFenceTest.d \
-        -Dtest.class.path=/Users/zeedh/slowcoders/jdk-rtgc/build/macosx-x86_64-client-fastdebug/test-support/jtreg_test_jdk_java_lang_ref_ReachabilityFenceTest_java/classes/0/java/lang/ref/ReachabilityFenceTest.d \
-        -Dtest.class.path.prefix=/Users/zeedh/slowcoders/jdk-rtgc/build/macosx-x86_64-client-fastdebug/test-support/jtreg_test_jdk_java_lang_ref_ReachabilityFenceTest_java/classes/0/java/lang/ref/ReachabilityFenceTest.d:/Users/zeedh/slowcoders/jdk-rtgc/test/jdk/java/lang/ref \
-        -Xmx768m \
-        -XX:MaxRAMPercentage=6.25 \
-        -Djava.io.tmpdir=/Users/zeedh/slowcoders/jdk-rtgc/build/macosx-x86_64-client-fastdebug/test-support/jtreg_test_jdk_java_lang_ref_ReachabilityFenceTest_java/tmp \
-        -ea \
-        -esa \
-        -Djava.library.path=/Users/zeedh/slowcoders/jdk-rtgc/build/macosx-x86_64-client-fastdebug/images/test/jdk/jtreg/native \
-        -Xint \
-        -Dpremature=false \
-        com.sun.javatest.regtest.agent.MainWrapper /Users/zeedh/slowcoders/jdk-rtgc/build/macosx-x86_64-client-fastdebug/test-support/jtreg_test_jdk_java_lang_ref_ReachabilityFenceTest_java/java/lang/ref/ReachabilityFenceTest.d/main.0.jta
+        -Dtest.classes=/Users/zeedh/slowcoders/jdk-rtgc/build/macosx-x86_64-client-fastdebug/test-support/jtreg_test_jdk_java_lang_ref_ReferenceEnqueue_java/classes/0/java/lang/ref/ReferenceEnqueue.d \
+        -Dtest.class.path=/Users/zeedh/slowcoders/jdk-rtgc/build/macosx-x86_64-client-fastdebug/test-support/jtreg_test_jdk_java_lang_ref_ReferenceEnqueue_java/classes/0/java/lang/ref/ReferenceEnqueue.d \
+        -Dtest.class.path.prefix=/Users/zeedh/slowcoders/jdk-rtgc/build/macosx-x86_64-client-fastdebug/test-support/jtreg_test_jdk_java_lang_ref_ReferenceEnqueue_java/classes/0/java/lang/ref/ReferenceEnqueue.d:/Users/zeedh/slowcoders/jdk-rtgc/test/jdk/java/lang/ref \
+        -classpath /Users/zeedh/slowcoders/jdk-rtgc/build/macosx-x86_64-client-fastdebug/test-support/jtreg_test_jdk_java_lang_ref_ReferenceEnqueue_java/classes/0/java/lang/ref/ReferenceEnqueue.d:/Users/zeedh/slowcoders/jdk-rtgc/test/jdk/java/lang/ref:/Users/zeedh/slowcoders/jdk-rtgc/jtreg-6.1/lib/javatest.jar:/Users/zeedh/slowcoders/jdk-rtgc/jtreg-6.1/lib/jtreg.jar \
+        ReferenceEnqueue
